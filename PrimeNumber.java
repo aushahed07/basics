5138808896bec553ab2f9e0adf2c7fd70ee7b1a7
Md. Ashraf Uddin Shahed <shahed@mislbd.com>
2023-01-20 23:47:49 +0600

    Prime number

---
+++ PrimeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = scanner.nextInt();
+        int[] numbers = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < arraySize; i++) {
+            System.out.println(isPrime(numbers[i]));
+        }
+    }
+
+    private static String isPrime(int number) {
+        for (int i = 2; i <= number/2; i++) {
+            if (number % i == 0) {
+                return "Not Prime";
+            }
+        }
+        return "Prime";
+    }
+}
